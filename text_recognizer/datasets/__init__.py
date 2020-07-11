4b23361b3747b07d1656eb99296888f89fbfdaa4
sambaths <sambath.s20@bimtech.ac.in>
2020-07-11 13:25:59 +0530

    #4

--- text_recognizer/datasets/__init__.py
+++ text_recognizer/datasets/__init__.py
@@ -1,2 +1,3 @@
 
 from .emnist_dataset import EmnistDataset
+from .emnist_lines_dataset import EmnistLinesDataset
--- text_recognizer/networks/__init__.py
+++ text_recognizer/networks/__init__.py
@@ -1,6 +1,7 @@
 """Neural network code modules."""
 from .mlp import mlp
 from .lenet import lenet
+from .line_cnn_all_conv import line_cnn_all_conv
 
 
 
