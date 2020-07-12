d115ba64a1de65488320e9f5d4182129ac719721
sambaths <sambath.s20@bimtech.ac.in>
2020-07-12 19:59:24 +0530

    8/Lab5/1

--- text_recognizer/datasets/__init__.py
+++ text_recognizer/datasets/__init__.py
@@ -1,3 +1,6 @@
 
 from .emnist_dataset import EmnistDataset
 from .emnist_lines_dataset import EmnistLinesDataset
+from .iam_dataset import IamDataset
+from .iam_lines_dataset import IamLinesDataset
+from .iam_paragraphs_dataset import IamParagraphsDataset 
