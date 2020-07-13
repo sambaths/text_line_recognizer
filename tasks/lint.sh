bad27f379af5e7cdbff10e72ef28c71c062aa306
sambaths <sambath.s20@bimtech.ac.in>
2020-07-13 22:53:48 +0530

    edited circleci config and lint.sh again #3

--- tasks/lint.sh
+++ tasks/lint.sh
@@ -7,20 +7,32 @@ FAILURE=false
 echo "safety"
 safety check -r requirements.txt || FAILURE=true
 
+if [ "$FAILURE" = true ]; then
+  echo "safety check failed"
+  
 echo "pylint"
 pylint api text_recognizer training || FAILURE=true
 
+if [ "$FAILURE" = true ]; then
+  echo "pylint failed failed"
+  
 echo "pycodestyle"
 pycodestyle api text_recognizer training || FAILURE=true
 
-echo "pydocstyle"
-pydocstyle api text_recognizer training || FAILURE=true
+if [ "$FAILURE" = true ]; then
+  echo "pydocstyle check failed"
 
 echo "mypy"
 mypy api text_recognizer training || FAILURE=true
 
+if [ "$FAILURE" = true ]; then
+  echo "mypy check failed"
+  
 echo "bandit"
 bandit -ll -r {api,text_recognizer,training} || FAILURE=true
+
+if [ "$FAILURE" = true ]; then
+  echo "bandit check failed"
 
 echo "shellcheck"
 shellcheck tasks/*.sh || FAILURE=true
